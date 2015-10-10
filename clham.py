eaf4fe337d4978809ff60d61a593dfc38e971271
Rhiannon Coppin <coppinr@gmail.com>
2015-10-10 15:51:24 -0700

    Tidied some things up.

--- clham.py
+++ clham.py
@@ -1,30 +1,87 @@
 """
 CLHAM
 
-Command-line (Canadian) HAM amateur radio question bank explorer
+Command-Line (Canadian) HAM:
+Test generator for the Industry Canada amateur radio question banks
+
 by Rhiannon Coppin, North Vancouver, B.C.
-Created April 7, 2015 for Basic question set.
-Updated Sept. 21, 2015 to accomodate Advanced question set as well.
-Tidied up Oct. 10, 2015.
+
+Updated: Oct. 10, 2015
+
+Quick Start:
+
+Usage: clham.py -i <input textfile>
+
+OPTIONAL arguments:
+
+       -a (use for advanced question set [DEFAULT is basic])
+       -m (mode: "exam" for exam silumator [DEFAULT is "all" questions])
+       -f (use to select Francais [DEFAULT landuage is English])
+
+Enter "q" anytime to quit.
+
+Notes:
+
+    The input text file can be the Industry Canada delimited question bank, or the output of "wrong answers" obtained from a previous run of this program.
+
+    The exam mode ("-m exam") runs through one pseudo-randomly chosen question from each section (though you can restrict which sections to pull from), resulting in a generated test of 50 questions for the Advanced set, and of 100 questions for the Basic set.
+
+    You are given the option after finishing the test, or even after entering "q" to quit, to save the questions you got wrong to a text file. You can then rerun that text file as input, and run a test from only your 'problem' questions.
+
+    The French translation of the intro and section labels is poor; the labelling of the section in English may also be questionable.
+
+Sample Usage:
+
+To run one or more entire sections from the Advanced Question Bank:
+
+    python clham.py -a -i amat_adv_quest/amat_adv_quest_delim.txt
+
+To run a simulated Advanced exam:
+
+    python clham.py -a -i amat_adv_quest/amat_adv_quest_delim.txt -m exam
+
+To run one or more entire sections from the Basic Question Bank:
+
+    python clham.py -i amat_basic_quest/amat_basic_quest_delim.txt
+
+To run a simulated Basic exam:
+
+    python clham.py -i amat_basic_quest/amat_basic_quest_delim.txt -m exam
+
+To run the set of questions you got wrong and save to a textfile (say, name "wrong_answers.txt") from a previous run this program:
+
+    python clham.py -i wrong_answers.txt
+
+To run the Industry Canada question banks in French instead, add the switch "-f" to any of the commands above
+Details:
 
 This command-line python utility reads in and presents various forms of the basic and advanbced question banks, which are made available as dual-language delimited text files from Industry Canada's website.
-The URLs of these file (zipped) at this time (Oct. 2015) is: http://apc-cap.ic.gc.ca/datafiles/amat_basic_quest.zip and http://apc-cap.ic.gc.ca/datafiles/amat_adv_quest.zip
+
+The URLs of these file (zipped) at this time (Oct. 2015) are:
+
+    http://apc-cap.ic.gc.ca/datafiles/amat_basic_quest.zip
+    http://apc-cap.ic.gc.ca/datafiles/amat_adv_quest.zip
+
 Locator URL on Industry Canada site: http://www.ic.gc.ca/eic/site/025.nsf/eng/h_00004.html
+
 Note: If the format of the file changes substantially, this program may need modification to function.
+
 The current schema of the text question bank is, as Industry Canada states:
 
 Each record has the following data, separated by ";"
-Question ID
-English Question
-Correct English Answer
-Incorrect English Answer 1
-Incorrect English Answer 2
-Incorrect English Answer 3
-French Question
-Correct French Answer
-Incorrect French Answer 1
-Incorrect French Answer 2
-Incorrect French Answer 3
+
+    Question ID
+    English Question
+    Correct English Answer
+    Incorrect English Answer 1
+    Incorrect English Answer 2
+    Incorrect English Answer 3
+    French Question
+    Correct French Answer
+    Incorrect French Answer 1
+    Incorrect French Answer 2
+    Incorrect French Answer 3
+
 
 """
 
@@ -294,7 +351,7 @@ class CLHAM:
     def save(self):
     	#self.__endtime = time.time()
 		print "Total time elapsed is: "+ self.get_time_elapsed()
-		print "You are leaving the CLHAM. Would you like to save the set of questions you got wrong to a text file to review or load into the CLHAM later? (Enter N for 'no' or the filename if yes, then press Enter)"
+		print "You are leaving the CLHAM. \nWould you like to save the set of questions you got wrong to a text file?\n(You can use that file as input into the CLHAM later.)\nEnter N or n for 'no,' or type in the filename if 'yes,' and then press Enter."
 		response = raw_input()
 		if response == '':
 			print "Goodbye."
